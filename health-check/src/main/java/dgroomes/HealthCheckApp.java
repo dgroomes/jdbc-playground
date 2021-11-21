379728013abfd25e369f56ec6cc82821e48a2c90
David Groomes <dwgroomes@gmail.com>
2021-11-21 12:48:03 -0600

    [health-check] Consolidate all code into "main" method
    This makes it quickest to digest for the reader.

--- health-check/src/main/java/dgroomes/HealthCheckApp.java
+++ health-check/src/main/java/dgroomes/HealthCheckApp.java
@@ -1,9 +1,7 @@
 package dgroomes;
 
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -12,44 +10,25 @@ import java.sql.SQLException;
  */
 public class HealthCheckApp {
 
-    private static final Logger log = LoggerFactory.getLogger(HealthCheckApp.class);
-
-    private static final String JDBC_URL = "jdbc:postgresql:postgres";
-
-    private final Connection connection;
-
-    public HealthCheckApp(Connection connection) {
-        this.connection = connection;
-    }
-
-    private static final String PREAMBLE = "Postgres health check status: ";
-
     public static void main(String... args) {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(JDBC_URL, "postgres", null);
-        } catch (SQLException e) {
-            log.error(PREAMBLE + "COULD NOT CONNECT ❌");
-            System.exit(0);
-        }
+        var log = LoggerFactory.getLogger(HealthCheckApp.class);
+        var logPreamble = "Postgres health check status: ";
+        var jdbcUrl = "jdbc:postgresql:postgres";
+        var username = "postgres";
 
-        var app = new HealthCheckApp(connection);
-        app.healthCheck();
-    }
+        try (var connection = DriverManager.getConnection(jdbcUrl, username, null);
+             var statement = connection.createStatement();
+             var resultSet = statement.executeQuery("SELECT 1")) {
 
-    private void healthCheck() {
-        try (var stmt = connection.createStatement();
-             var rs = stmt.executeQuery("SELECT 1")) {
-
-            rs.next();
-            int found = rs.getInt(1);
+            resultSet.next();
+            int found = resultSet.getInt(1);
             if (found == 1) {
-                log.info(PREAMBLE + "UP ✅");
+                log.info(logPreamble + "UP ✅");
             } else {
-                log.error(PREAMBLE + "UNEXPECTED RESPONSE ❌");
+                log.error(logPreamble + "UNEXPECTED RESPONSE ❌");
             }
         } catch (SQLException e) {
-            log.error(PREAMBLE + "EXCEPTION ❌", e);
+            log.error(logPreamble + "COULD NOT CONNECT ❌");
         }
     }
 }
